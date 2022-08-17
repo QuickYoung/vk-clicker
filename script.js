85217b533fd99059cca454a8c515108fec6aa13a
Roma Romanovv <moto.pers2007@gmail.com>
2022-08-18 03:50:44 +0400

    new commit

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-"use strict";
-onkeydown = e => e.key == ' ' ? clicksCountSpan.innerText++ : 0
+if (!localStorage.click) localStorage.click = 0
+clicksCountSpan.innerText = localStorage.click
+onkeydown = (e) =>
+  e.key == ' ' ? (clicksCountSpan.innerText++, localStorage.click++) : 0
